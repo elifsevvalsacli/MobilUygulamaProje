616d5f73cdcbb1d3c344ee61354f8fd01446f3fd
elifsevvalsacli <90870934+elifsevvalsacli@users.noreply.github.com>
2024-12-21 23:39:57 +0300

    index.js oluşturuldu.

---
+++ screens/index.js
@@ -0,0 +1,6 @@
+// screens/index.js
+export { default as HomeScreen } from './HomeScreen';
+export { default as LoginScreen } from './LoginScreen';
+export { default as RegisterScreen } from './RegisterScreen';
+export { default as AdminLoginScreen } from './AdminLoginScreen';
+export { default as AdminDashboard } from './AdminDashboard';
