7c0a233f168596920d92e2a59e6bdc13365144cc
Dilay Kal <104075669+dilaykal@users.noreply.github.com>
2024-12-22 23:55:50 +0300

    düzeltme yapıldı

--- App.js
+++ App.js
@@ -2,6 +2,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AdminDashboard from './screens/AdminDashboard'; // AdminDashboard dosyanızın doğru yolu
+import GuidelineCreate from './screens/GuidelineCreate';
+import DataEntry from './screens/DataEntry';
+import GuidelineSearch from './screens/GuidelineSearch';
+import PatientTracking from './screens/PatientTracking';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {
   HomeScreen,
   LoginScreen,
